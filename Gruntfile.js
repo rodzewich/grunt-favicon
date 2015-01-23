86c58f0d8e482051cc3b4d8e1d08a876c4493a9b
rodzewich <rodzewich.oleg@gmail.com>
2015-01-23 13:09:24 +0300

    -

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,10 +5,23 @@
 
 module.exports = function (grunt) {
 
+    // These plugins provide necessary tasks.
     grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'src/*.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
         uglify: {
             options: {
                 banner: grunt.file.read("src/banner.txt")
@@ -20,8 +33,21 @@ module.exports = function (grunt) {
                     src    : "src/*.js"
                 }]
             }
+        },
+        // Build test data.
+        favicon: grunt.file.readJSON("tests/fixtures.json"),
+        // Unit tests.
+        nodeunit: {
+            tests: ['tests/*_test.js']
+        },
+        // Clean temp files.
+        clean: {
+            tests: ['tests/dest']
         }
     });
+
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('src');
 
     grunt.registerTask("default", "Build package.", ["uglify:tasks"]);
 
